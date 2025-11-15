ac0de180fbe0ca8c5a7dc3d987a56cbcfcdc4971
JiruGutema <jirudagutema@gmail.com>
2025-11-15 22:54:44 +0300

    feat(program.cs): add main file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program 
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        { // creating the object of Identifiers class
+            Identifiers identifiers = new Identifiers();
+            identifiers.IdentifiersMethod();
+            // creating the object of Variables class
+            Variables variables = new Variables();
+            variables.VariablesMethod();
+
+            // creating the object of DataTypes class
+            DataTypes dataTypes = new DataTypes();
+            dataTypes.DataTypesMethod();
+
+            // creating the object of Literals class
+            Literals literals = new Literals();
+            literals.LiteralsMethod();
+        }
+    }
+}
