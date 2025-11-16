aa48665924d97a426fd8e1b23750c39a96deef6b
JiruGutema <jirudagutema@gmail.com>
2025-11-16 12:05:11 +0300

    feat(obj): update for obj

--- obj/Debug/net9.0/CsByExample.AssemblyInfo.cs
+++ obj/Debug/net9.0/CsByExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsByExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffbae6f60fe0a4c2eda2c5065981fde10915ca51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48cada01ead40fbd192a107c3b23ae65679fea13")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsByExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsByExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
